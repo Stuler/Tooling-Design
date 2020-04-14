05b36e7ff023a88aece54fd83a47eb88f42aae44
Stuler <peter.jurek@gmail.com>
2020-04-14 04:22:23 +0200

    Create necking_die.py

---
+++ necking_die.py
@@ -0,0 +1,9 @@
+# Script that generates basic necking die dimensions
+# Each die is ought o be an object inherited from a CAN class
+
+
+class Can:
+    def __init__(
+            ):
+        self.can_dia = can_dia
+        self.can_height = can_height
